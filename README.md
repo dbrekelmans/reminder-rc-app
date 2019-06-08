86375a8a241eca64ac120c0874eafc1e2b977ada
Daniël Brekelmans <dbrekelmans@live.nl>
2019-06-08 13:52:03 +0200

    Initialize app

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# rocketchat-remindBot
-Remind bot for rocketchat that adds a /remind command
+# remindBot
+Supports setting reminders with a /remind command
+
+## Development
+
+### Environment
+* `docker-compose up -d`
+* `docker-compose exec node bash`
+
+### Deploying app to rocketchat server
+`rc-apps deploy --url http://rocketchat:3000 --username admin --password admin --update`
---
+++ src/RemindBotApp.ts
@@ -0,0 +1,9 @@
+import {IAppAccessors, ILogger,} from '@rocket.chat/apps-engine/definition/accessors';
+import {App} from '@rocket.chat/apps-engine/definition/App';
+import {IAppInfo} from '@rocket.chat/apps-engine/definition/metadata';
+
+export class RemindBotApp extends App {
+    constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
+        super(info, logger, accessors);
+    }
+}
